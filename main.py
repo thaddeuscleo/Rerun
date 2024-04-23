afd4286e70f82c9a723b7bcac023d045a2e79155
thaddeuscleo <thaddeuscleo@gmail.com>
2024-04-23 14:41:07 +0700

    feat: implement modes

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def lines_to_rerun(json_data) -> rr.LineStrips2D:
     line_ids = []
     for line in json_data["lines"]:
         lines.append([line["start"], line["end"]])
-        line_ids.append(line["id"])
+        line_ids.append(f'{line["id"]} (In: {line["in_count"]}, Out: {line["out_count"]})')
 
     return rr.LineStrips2D(lines, labels=line_ids)
 
@@ -82,11 +82,46 @@ def images_to_rerun(json_data):
     object_name = json_data["video_frame"]["id"]
     image = load_image_from_minio(minio_cli, bucket_name, object_name)
 
-    return rr.Image(image)
+    return rr.Image(image).compress(jpeg_quality=30)
 
+# def queue_time_to_rerun(json_data):
+#     queue_times = []
+
+#     for queue in json_data["queue_time"]:
+#         queue_times.append(queue)
+
+def licence_plates_to_rerun(json_data):
+    boxes = []
+    labels = []
+
+    if json_data["licence_plates"] is not None:
+        for obj in json_data["licence_plates"]:
+            boxes.append(obj["region"]["xyxy"])
+            labels.append(f"{obj['text']}")
+    else:
+        return
+    
+    return rr.Boxes2D(
+        array=boxes,
+        labels=labels,
+        array_format=rr.Box2DFormat.XYXY,
+    )
+
+
+def crowd_to_rerun(json_data):
+    points = []
+
+    for coordinate in json_data["points"]:
+        points.append(coordinate)
+
+    return rr.Points2D(
+        positions=points,
+    )
 
 rr.init("rerun_example_box2d", spawn=True)
 
+
+MODE = 'LPR' # OBJ, LPR, CC
 
 try:
     while True:
@@ -105,15 +140,32 @@ try:
             # Process the received message
             pred = json.loads(msg.value().decode('utf-8'))
 
-            objs = objects_to_rerun(pred)
-            zns = zones_to_rerun(pred)
-            lns = lines_to_rerun(pred)
-            img = images_to_rerun(pred)
+            if MODE == 'OBJ':
+                objs = objects_to_rerun(pred)
+                zns = zones_to_rerun(pred)
+                lns = lines_to_rerun(pred)
+                img = images_to_rerun(pred)
 
-            rr.log("object_detection/bbox", objs)
-            rr.log("object_detection/zones", zns)
-            rr.log("object_detection/lines", lns)
-            rr.log("object_detection/images", img)
+                rr.log("object_detection/zones", zns)
+                rr.log("object_detection/lines", lns)
+                rr.log("object_detection/images", img)
+                rr.log("object_detection/bbox", objs)
+
+            elif MODE == 'LPR':
+                lcp = licence_plates_to_rerun(pred)
+                img = images_to_rerun(pred)
+
+                rr.log("licence_plate_recognition/images", img)
+                rr.log("licence_plate_recognition/bbox", lcp)
+
+            elif MODE == 'CC':
+                img = images_to_rerun(pred)
+                ccp = crowd_to_rerun(pred)
+
+                rr.log("crowd_counting/images", img)
+                rr.log("crowd_counting/points", ccp)
+
+
 
 except KeyboardInterrupt:
     pass
